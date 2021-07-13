adbb35692a5c651ca243cd65305b6724d850322c
haidyelnahass <haidyelnahass@hotmail.com>
2021-07-13 10:36:02 +0200

    adjustments to some comments in code

--- ProjectThree/Program.cs
+++ ProjectThree/Program.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Xml;
 
-//on working with a txt file and converting it to string
 
 namespace ProjectThree
 {
@@ -148,7 +146,6 @@ namespace ProjectThree
             string fileDestination = @"C:\Users\Haidy\Downloads\DS_Project\";
             string[] files = System.IO.Directory.GetFiles(fileDestination, "*.xml");
             string inputText = System.IO.File.ReadAllText(files[0]);
-            //Console.WriteLine($"[{inputText[0]}]");
 
             //Stack for Tags, to check for consistency.
             Stack<StackElement> Tagstack = new Stack<StackElement>();
@@ -167,7 +164,6 @@ namespace ProjectThree
             Console.WriteLine(inputText.Length);
             while (loopIndex != inputText.Length)
             {
-                // Console.WriteLine($"Currently [{inputText[loopIndex]} , {loopIndex}]");
                 if (inputText[loopIndex] == '\r' || inputText[loopIndex] == '\n')
                 {
                     loopIndex++;
@@ -191,7 +187,6 @@ namespace ProjectThree
                 loopIndex++;
             }
 
-            //Console.WriteLine(sentence);
             //empty array that will contain all elements of XML seperated the right way
             //we first initialize it to the length of the previous XML array
             //and if we need to resize it along the way, we will
@@ -214,11 +209,11 @@ namespace ProjectThree
             //we want to store it in final array that way but not in the tag stack
             string sentence2 = "";
             bool isLongTag = false;
-            //Console.WriteLine(sentence);
+
+
             j = 0;
             while (j != sentence.Trim().Length)
             {
-                //Console.WriteLine($"Current j is {j}");
 
                 //check for tag
                 if (sentence[j] == '<')
@@ -269,7 +264,6 @@ namespace ProjectThree
                         else
                         {
                             OpeningTagNumber++;
-                            //Console.WriteLine($"{temp} tag number is {OpeningTagNumber}");
                         }
 
 
@@ -302,7 +296,6 @@ namespace ProjectThree
                             XML_index++;
                         }
 
-                        //Console.WriteLine($"Temp is currently {temp}");
 
                         //empty temp string after adding it to array.
                         temp = "";
@@ -344,11 +337,10 @@ namespace ProjectThree
 
                                 // Console.Write("Found empty Stack, Adding new Opening Tag\n");
 
-                                //Console.WriteLine($"Index to add tag in is {OpeningTagNumber}");
+                              
 
                                 resizeArraybyOne(ref XML_array, XML_array.Length, ref OpeningTagNumber, temp);
                                 XML_index++;
-                                // Console.Write("Tag Added!\n");
                                 XML_array[XML_index] = temp.Substring(0, 1) + "/" + temp.Substring(1);
                                 XML_index++;
                                 temp = "";
@@ -362,26 +354,18 @@ namespace ProjectThree
 
                                 string temp2 = "";
 
-                                //Console.WriteLine(temp);
 
                                 //searching for opening tag in stack.
 
                                 bool isFound = findTagInStack(Tagstack, temp);
-                                // Console.Write($"Searching for tag {temp} now.\n");
-                                // if(Tagstack.Count == 0)
-                                // {
-                                //     Console.Write("Emptyy!\n");
-                                // }
 
                                 //if found, we add closing tags for all current opening tags till we reach it.
 
                                 if (isFound)
                                 {
-                                    // Console.Write("Found Tag!\n");
                                     while (Tagstack.Count != 0 && Tagstack.Peek().getSentence().Trim() != temp.Trim())
                                     {
-                                        // Console.Write($"[{Tagstack.Peek().getSentence()} , {Tagstack.Peek().getIndex()}]\n");
-                                        // Console.Write($"[{temp}]\n");
+                                        
 
                                         //adding closing tags to current tags until we find
                                         //opening tag for current closing tag
@@ -390,14 +374,12 @@ namespace ProjectThree
                                         int index2 = Tagstack.Peek().getIndex();
                                         resizeArraybyOne(ref XML_array, XML_array.Length, ref index2, temp2.Substring(0, 1) + "/" + temp2.Substring(1));
                                         XML_index++;
-                                        // Console.Write($"Tag Added! {temp2} at {index2}\n");
                                         OpeningTagNumber++;
 
                                         //***********************//
 
 
                                         int current_index = Tagstack.Peek().getIndex() + 1;
-                                        // Console.WriteLine($"{Tagstack.Peek().getSentence()} its index is {current_index}");
                                         Tagstack.Pop();
                                         if (Tagstack.Count != 0) //test if also the last element in the final array is a string or not
                                                                  //before increasing it.
@@ -406,16 +388,13 @@ namespace ProjectThree
                                             int index = Tagstack.Peek().getIndex(); //index of current tag
                                             if (XML_array[index - 1][0] == '<')
                                             {
-                                                // Console.WriteLine($"{Tagstack.Peek().getSentence()} indddex is {index}");
                                                 Tagstack.Peek().setIndex(current_index);
-                                                // Console.WriteLine($"{Tagstack.Peek().getSentence()} iiindex is {Tagstack.Peek().getIndex()}");
                                             }
                                         }
                                     }
 
                                     if (Tagstack.Peek().getSentence().Trim() == temp.Trim())
                                     {
-                                        //Console.WriteLine($"Temp is currently {temp}");
                                         if (XML_index != XML_array.Length)
                                         {
                                             XML_array[XML_index] = temp.Substring(0, 1) + "/" + temp.Substring(1); ;
@@ -428,16 +407,13 @@ namespace ProjectThree
                                             XML_index++;
                                         }
                                         int current_index = Tagstack.Peek().getIndex() + 1;
-                                        // Console.WriteLine($"{Tagstack.Peek().getSentence()} index is {current_index}");
                                         Tagstack.Pop();
                                         if (Tagstack.Count != 0)
                                         {
                                             int index = Tagstack.Peek().getIndex();
                                             if (XML_array[index - 1][0] == '<')
                                             {
-                                                // Console.WriteLine($"{Tagstack.Peek().getSentence()} inddex is {index}");
                                                 Tagstack.Peek().setIndex(current_index);
-                                                // Console.WriteLine($"{Tagstack.Peek().getSentence()} iindex is {Tagstack.Peek().getIndex()}");
                                             }
                                         }
                                         temp = "";
@@ -449,13 +425,10 @@ namespace ProjectThree
 
                                 else //empty stack, so opening tag is missing.
                                 {
-                                    // Console.WriteLine("Tag not found, unfortunately");
 
                                     //add opening tag and closing tag.
-                                    // Console.WriteLine($"Index to add tag in is {OpeningTagNumber}");
                                     resizeArraybyOne(ref XML_array, XML_array.Length, ref OpeningTagNumber, temp);
                                     XML_index++;
-                                    // Console.WriteLine("Tag Added!");
                                     XML_array[XML_index] = temp.Substring(0, 1) + "/" + temp.Substring(1);
                                     XML_index++;
                                     temp = "";
@@ -467,7 +440,6 @@ namespace ProjectThree
                         }
                         else
                         {
-                            //Console.WriteLine($"Temp is currently {temp}");
                             if (XML_index != XML_array.Length)
                             {
                                 XML_array[XML_index] = temp.Substring(0, 1) + "/" + temp.Substring(1); ;
@@ -480,16 +452,13 @@ namespace ProjectThree
                                 XML_index++;
                             }
                             int current_index = Tagstack.Peek().getIndex() + 1;
-                            // Console.WriteLine($"{Tagstack.Peek().getSentence()} indeex is {current_index}");
                             Tagstack.Pop();
                             if (Tagstack.Count != 0)
                             {
                                 int index = Tagstack.Peek().getIndex();
                                 if (XML_array[index - 1][0] == '<')
                                 {
-                                    // Console.WriteLine($"{Tagstack.Peek().getSentence()} index iss {index}");
                                     Tagstack.Peek().setIndex(current_index);
-                                    // Console.WriteLine($"{Tagstack.Peek().getSentence()} indexx is {Tagstack.Peek().getIndex()}");
                                 }
                             }
                             temp = "";
@@ -500,7 +469,6 @@ namespace ProjectThree
                 }
                 else
                 {
-                    // Console.WriteLine($"Currently {sentence}");
                     //not an opening tag or a closing tag
                     //just a sentence
                     //needed for the final array
@@ -511,7 +479,6 @@ namespace ProjectThree
                         // {
                         //     temp+="\n";
                         // }
-                        //Console.WriteLine($"Temp is currently {temp} and j is {j}");
                         j++;
                     }
 
@@ -531,15 +498,10 @@ namespace ProjectThree
                     }
                     OpeningTagNumber++;
                     StackElement element = Tagstack.Peek();
-                    // Console.WriteLine($"Currently increasing this one {element.getSentence()}");
                     int index = element.getIndex() + 2;
-                    // Console.WriteLine($"{index} is the index of {Tagstack.Peek().getSentence()}");
-                    // Console.WriteLine($"XML INDEX IS {XML_index}");
                     if (XML_index == index)
                     {
-                        // Console.WriteLine("Adding");
                         element.setIndex((index));
-
                     }
                     Tagstack.Pop();
                     Tagstack.Push(element);
@@ -550,7 +512,6 @@ namespace ProjectThree
 
 
 
-            // Console.WriteLine("END");
 
             if (Tagstack.Count == 0)
             {
@@ -565,28 +526,19 @@ namespace ProjectThree
                     int index = Tagstack.Peek().getIndex();
                     Console.WriteLine($"{temp} index is {index} and Length is {XML_array.Length}");
                     resizeArraybyOne(ref XML_array, XML_array.Length, ref index, temp.Substring(0, 1) + "/" + temp.Substring(1));
-                    // if (XML_index != XML_array.Length)
-                    // {
-                    //     XML_array[XML_index] = temp.Substring(0, 1) + "/" + temp.Substring(1); ;
-                    //     XML_index++;
-                    // }
-                    // else
-                    // {
-                    //     resizeArraytoDouble(ref XML_array, XML_array.Length);
-                    //     XML_array[XML_index] = temp.Substring(0, 1) + "/" + temp.Substring(1); ;
-                    //     XML_index++;
-                    // }
+                    
+
                     OpeningTagNumber++;
                     int current_index = Tagstack.Peek().getIndex() + 1;
                     string current_Tag = Tagstack.Peek().getSentence();
-                    // Console.WriteLine($"{Tagstack.Peek().getSentence()} indeex is {current_index}");
                     Tagstack.Pop();
+
+                    
                     if (Tagstack.Count != 0)
                     {
                         int ind = Tagstack.Peek().getIndex();
                         if (XML_array[ind - 1][0] == '<')
                         {
-                            // Console.WriteLine($"{Tagstack.Peek().getSentence()} index iss {index}");
                             if (current_Tag == XML_array[XML_index])
                             {
                                 Tagstack.Peek().setIndex(current_index);
@@ -595,7 +547,6 @@ namespace ProjectThree
                             {
                                 Tagstack.Peek().setIndex(XML_index);
                             }
-                            // Console.WriteLine($"{Tagstack.Peek().getSentence()} indexx is {Tagstack.Peek().getIndex()}");
                         }
                     }
                     XML_index++;
